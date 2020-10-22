666d5f49de130b522d228283d50afb17f27941e7
AndreMarcos <andre92marcos@gmail.com>
2020-10-22 15:52:47 +0100

    Created a script for general skills plumbing

---
+++ challenges/picoCTF_2019/general_skills/plumbing/run.py
@@ -0,0 +1,16 @@
+from pwn import *
+sh = remote('jupiter.challenges.picoctf.org', 41521)
+
+binary = sh.recvuntil('Input:\n').decode('UTF-8').split('\n')[2].split(' ')[3:-3]
+binary = ''.join(map(lambda x: chr(int(x, 2)), binary))
+sh.sendline(binary)
+
+octa = sh.recvuntil('Input:\n').decode('UTF-8').split('\n')[0].split(' ')[5:-3]
+octa = ''.join(map(lambda x: chr(int(x, 8)), octa))
+sh.sendline(octa)
+
+hexa = sh.recvuntil('Input:\n').decode('UTF-8').split('\n')[0].split(' ')[4:-3]
+hexa =  bytes.fromhex(hexa[0]).decode('UTF-8') 
+sh.sendline(hexa)
+flag = sh.recvall().decode('UTF-8').split('\n')[1].split(' ')[1]
+print(flag)
