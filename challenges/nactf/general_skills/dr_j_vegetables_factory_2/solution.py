4b0eb802baf47a74a6c6b86727d62ca09ebbf1bb
Andre Marcos <andre92marcos@gmail.com>
2020-11-27 10:53:52 -0500

    Didnt finished dr_j_vegetables_factory

--- challenges/nactf/general_skills/dr_j_vegetables_factory_2/solution.py
+++ challenges/nactf/general_skills/dr_j_vegetables_factory_2/solution.py
@@ -5,4 +5,10 @@ sh = remote('challenges.ctfd.io' , 30267)
 
 sh.recvuntil('Enter 1, 2, 3, 4, or 5.\n')
 
+sh.sendline("2")
 
+sh.recvuntil('order:\n\n')
+vegetables = sh.recvline().decode('utf-8')
+#print(vegetables)
+print(vegetables.strip().split(','))
+#sh.interactive()
